00afa7366f47fea641a3445db6e11b150ee286a2
Kai Wan <1540565996@qq.com>
2018-09-11 13:07:30 +0800

    the first homework finished

--- CSharpHomework/homework1/text1/Program.cs
+++ CSharpHomework/homework1/text1/Program.cs
@@ -10,6 +10,16 @@ namespace text1
     {
         static void Main(string[] args)
         {
+            string s = "";
+            double num1 = 0;
+            double num2 = 0;
+            Console.Write("Please input num1 :");
+            s = Console.ReadLine();
+            num1 = Double.Parse(s);
+            Console.Write("Please input num2 :");
+            s = Console.ReadLine();
+            num2 = Double.Parse(s);
+            Console.WriteLine("The multiplication of num1 and num2 is :" + (num1 * num2));
         }
     }
 }
