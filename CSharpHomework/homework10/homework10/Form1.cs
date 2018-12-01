8295af1b3ba9193ddb7e01fdd33f4299a7210512
Kai Wan <1540565996@qq.com>
2018-12-01 11:57:44 +0800

    the last homework finished

--- CSharpHomework/homework10/homework10/Form1.cs
+++ CSharpHomework/homework10/homework10/Form1.cs
@@ -13,11 +13,13 @@ using System.Xml.Serialization;
 
 using System.Xml;
 using System.Xml.Xsl;
+using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Data.OleDb;
 
 
 
-namespace homework8
+namespace homework10
 {
 
     public partial class Form1 : Form
@@ -25,9 +27,30 @@ namespace homework8
         public Form1()
         {
             InitializeComponent();
-            
+            Init();
         }
 
+        void Init()
+        {
+            try
+            {
+                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句                
+                            //在这里使用代码对数据库进行增删查改
+                string strMySql = "SELECT * FROM orderdetails";
+                daAdapter = new MySqlDataAdapter(strMySql, conn);
+                daAdapter.Fill(daSet);
+                listBox1.Items.Add(daSet.Tables[0].Rows[0][1]);
+                dataUpdate();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
         //OrderDetails
         public class OrderDetails
         {
@@ -39,9 +62,9 @@ namespace homework8
             public string orderPhone;
             public OrderDetails()
             {
-                OrderDetails.orders++;                
-            }            
-            
+                OrderDetails.orders++;
+            }
+
             public void change(string orName, string orOwner, string orMoney)
             {
                 this.orderName = orName;
@@ -50,13 +73,15 @@ namespace homework8
             }
         }
 
+        static String connetStr = "server=127.0.0.1;port=3306;user=root;password=root; database=csharphomework;";
+        MySqlConnection conn = new MySqlConnection(connetStr);
 
         //Order
         public class Order
         {
             public List<OrderDetails> orderList = new List<OrderDetails>();
 
-        }       
+        }
 
         public void showError(string s)
         {
@@ -94,7 +119,7 @@ namespace homework8
         }
         bool ifRepeat(string s)
         {
-            for(int i=0;i<order.orderList.Count;i++)
+            for (int i = 0; i < order.orderList.Count; i++)
             {
                 if (order.orderList[i].orderNumber == s)
                     return false;
@@ -105,7 +130,7 @@ namespace homework8
         }
         bool ifRight(string s)
         {
-            foreach(char ichar in s)
+            foreach (char ichar in s)
             {
                 if (ichar < 48 || ichar > 57)
                     return false;
@@ -126,15 +151,40 @@ namespace homework8
                 string num1 = num.Substring(0, 8);
 
 
-                if (s == num1  && ifRepeat(num) && ifRight(form2.textBox5.Text))
+                if (s == num1 && ifRepeat(num) && ifRight(form2.textBox5.Text))
                 {
-                    e.orderName = form2.textBox1.Text;
-                    e.orderOwner = form2.textBox2.Text;
-                    e.moneyNumber = form2.textBox3.Text;
-                    e.orderNumber = form2.textBox4.Text;
-                    e.orderPhone = form2.textBox5.Text;
-                    order.orderList.Add(e);
-                    listBox1.Items.Add(e.orderName);
+                    //e.orderName = form2.textBox1.Text;
+                    //e.orderOwner = form2.textBox2.Text;
+                    //e.moneyNumber = form2.textBox3.Text;
+                    //e.orderNumber = form2.textBox4.Text;
+                    //e.orderPhone = form2.textBox5.Text;
+                    //order.orderList.Add(e);
+                    listBox1.Items.Add(form2.textBox1.Text);
+
+                    try
+                    {
+                        conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句                
+                                    //在这里使用代码对数据库进行增删查改
+                        string strMySql = "SELECT * FROM orderdetails";
+                        daAdapter = new MySqlDataAdapter(strMySql, conn);
+                        daAdapter.Fill(daSet);
+                        DataRow row = daSet.Tables[0].NewRow();
+                        row[0] = form2.textBox4.Text;
+                        row[1] = form2.textBox1.Text;
+                        row[2] = form2.textBox2.Text;
+                        row[3] = form2.textBox5.Text;
+                        row[4] = Int32.Parse(form2.textBox3.Text);
+                        daSet.Tables[0].Rows.Add(row);
+                        dataUpdate();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
                 }
                 else
                 {
@@ -142,11 +192,11 @@ namespace homework8
                     return;
                 }
             }
-            catch
+            catch (MySqlException ex)
             {
-                showError("错误的信息！");
+                Console.WriteLine(ex.Message);
             }
-                     
+
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -173,17 +223,34 @@ namespace homework8
             form2.textBox4.Clear();
             form2.textBox5.Clear();
         }
-        
-        
+
+
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            for (int i = 0; i < order.orderList.Count; i++)
+         
+            try
             {
-                if (order.orderList[i].orderName  == listBox1.SelectedItem.ToString())
+                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句                
+                            //在这里使用代码对数据库进行增删查改
+                string strMySql = "SELECT * FROM orderdetails";
+                daAdapter = new MySqlDataAdapter(strMySql, conn);
+                daAdapter.Fill(daSet);
+                for (int i = 0; i < daSet.Tables[0].Rows.Count; i++)
                 {
-                    order.orderList.RemoveAt(i);                  
+                    if(daSet.Tables[0].Rows[i][1].ToString() == listBox1.SelectedItem.ToString())
+                    {
+                        daSet.Tables[0].Rows[i].Delete();
+                    }
                 }
+                dataUpdate();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
             }
             if (listBox1.SelectedIndex > -1)
                 listBox1.Items.Remove(listBox1.SelectedItem);
@@ -202,13 +269,31 @@ namespace homework8
             tb1.Location = new Point(10, 30);
             tb1.Size = new System.Drawing.Size(120, 140);
             form4.Controls.Add(tb1);
-            tb1.Text = "订单号： " + order.orderList[i].orderNumber.ToString() + "\r\n" +
-                "订单名称：" + order.orderList[i].orderName + "\r\n" +
-                "用户名称：" + order.orderList[i].orderOwner + "\r\n" +
-                "订单金额：" + order.orderList[i].moneyNumber.ToString() + "\r\n" +
-                "用户号码：" + order.orderList[i].orderPhone;
-            form4.ShowDialog();
-           
+            try
+            {
+                conn.Open();//打开通道，建立连接，可能出现异常,使用try catch语句                
+                            //在这里使用代码对数据库进行增删查改
+                string strMySql = "SELECT * FROM orderdetails";
+                daAdapter = new MySqlDataAdapter(strMySql, conn);
+                daAdapter.Fill(daSet);
+
+                tb1.Text = "订单号： " + daSet.Tables[0].Rows[i][0] + "\r\n" +
+              "订单名称：" + daSet.Tables[0].Rows[i][1] + "\r\n" +
+              "用户名称：" + daSet.Tables[0].Rows[i][2] + "\r\n" +
+              "订单金额：" + daSet.Tables[0].Rows[i][4].ToString() + "\r\n" +
+              "用户号码：" + daSet.Tables[0].Rows[i][3];
+                form4.ShowDialog();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -219,15 +304,15 @@ namespace homework8
             form3.AcceptButton = form3.button1;
 
             int selectNum = 0;
-            if(form3.listBox1.SelectedItem.ToString() == "订单号")
+            if (form3.listBox1.SelectedItem.ToString() == "订单号")
             {
                 selectNum = 1;
             }
-            else if(form3.listBox1.SelectedItem.ToString() == "订单名称")
+            else if (form3.listBox1.SelectedItem.ToString() == "订单名称")
             {
                 selectNum = 2;
             }
-            else if(form3.listBox1.SelectedItem.ToString() == "用户姓名")
+            else if (form3.listBox1.SelectedItem.ToString() == "用户姓名")
             {
                 selectNum = 3;
             }
@@ -235,6 +320,7 @@ namespace homework8
             {
                 selectNum = 4;
             }
+
 
             switch (selectNum)
             {
@@ -267,23 +353,7 @@ namespace homework8
                     }
                     break;
                 case 0:
-                    Form form4 = new Form();
-                    Button button1 = new Button();
-                    Label label1 = new Label();
-
-                    button1.Text = "OK";
-                    button1.Location = new Point(200, 50);
-                    label1.Text = "请选择要查询的订单！";
-                    label1.Location = new Point(20, 30);
-                    form4.Controls.Add(button1);
-                    form4.Controls.Add(label1);
-                    button1.DialogResult = DialogResult.Cancel;
-                    form4.CancelButton = button1;
-
-                    form4.ShowDialog();
-
-                    if (form4.DialogResult == DialogResult.Cancel)
-                        form4.Dispose();
+                    showError("请选择要查询的订单！");
                     break;
 
             }
@@ -333,25 +403,25 @@ namespace homework8
             ser.Serialize(fs, order);
             fs.Close();
         }
-        public void Export(ref Order order,string filePath)
+        public void Export(ref Order order, string filePath)
         {
-            XmlSerializer xmlser = new XmlSerializer(typeof(Order));           
+            XmlSerializer xmlser = new XmlSerializer(typeof(Order));
             XmlSerializer(xmlser, filePath, ref order);
         }
 
         public string Import(string xmlFileName)
-        {            
+        {
             string xml = File.ReadAllText(xmlFileName);
             return xml;
         }
-        
-        private void GetHtmlByXml (string xmlPath,string xslPath)
-        {           
+
+        private void GetHtmlByXml(string xmlPath, string xslPath)
+        {
 
             XslCompiledTransform trans = new XslCompiledTransform();
             trans.Load(xslPath);
             trans.Transform(xmlPath, "my.html");
-            
+
         }
         private void button5_Click(object sender, EventArgs e)
         {
@@ -365,7 +435,20 @@ namespace homework8
             catch
             {
                 showError("发生错误了！");
-            }           
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void dataUpdate()
+        {
+            MySqlCommandBuilder cmdBuilder = new MySqlCommandBuilder(daAdapter);
+            daAdapter.Update(daSet);
         }
     }
 }
---
+++ CSharpHomework/homework10/homework10/Form3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework10
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            initListBox();
+        }
+
+        protected void initListBox()
+        {
+            listBox1.Items.Add("订单号");
+            listBox1.Items.Add("订单名称");
+            listBox1.Items.Add("用户姓名");
+            listBox1.Items.Add("订单金额");
+        }      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
